63ee25c2a979d5fcd3be9b27d928e9f6034ab518
niexq <niexq@firstgrid.cn>
2021-08-03 11:54:01 +0800

    feat: qiankun配置运行时生命周期钩子（可选）

--- src/app.tsx
+++ src/app.tsx
@@ -81,3 +81,18 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
     ...initialState?.settings,
   };
 };
+
+export const qiankun = {
+  // 应用加载之前
+  async bootstrap(props) {
+    console.log('app1 bootstrap', props);
+  },
+  // 应用 render 之前触发
+  async mount(props) {
+    console.log('app1 mount', props);
+  },
+  // 应用卸载之后触发
+  async unmount(props) {
+    console.log('app1 unmount', props);
+  },
+};
